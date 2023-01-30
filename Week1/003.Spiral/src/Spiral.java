acf0e7a36f793c7c9cd659b3bdbeae94bbe70455
Meindert Kempe <mail@mkempe.com>
2023-01-30 15:09:58 +0100

    Week 1.3 Spiral

--- Week1/003.Spiral/src/Spiral.java
+++ Week1/003.Spiral/src/Spiral.java
@@ -9,9 +9,11 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Spiral extends Application {
+    private Canvas canvas;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Spiral");
@@ -20,10 +22,29 @@ public class Spiral extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+        graphics.scale(1, -1);
+
+        double res = 0.001;
+        double scale = 10;
+        double x1, x2, y1, y2;
+        for (double i = 0; i < Math.PI * 10; i += res) {
+            x1 = fx(i);
+            x2 = fx(i + res);
+            y1 = fy(i);
+            y2 = fy(i + res);
+            graphics.draw(new Line2D.Double(x1 * scale, y1 * scale, x2 * scale, y2 * scale));
+        }
     }
-    
-    
-    
+
+    private double fx(double t) {
+        return t * Math.cos(t);
+    }
+
+    private double fy(double t) {
+        return t * Math.sin(t);
+    }
+
     public static void main(String[] args) {
         launch(Spiral.class);
     }
