70c9b41bd7a1720de197844fe8574f1c40fd4e55
Meindert Kempe <mail@mkempe.com>
2023-01-30 16:14:09 +0100

    Week 1.5 Spirograph

--- Week1/005.Spirograph/src/Spirograph.java
+++ Week1/005.Spirograph/src/Spirograph.java
@@ -1,10 +1,15 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
+
 import javafx.application.Application;
+
 import static javafx.application.Application.launch;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,40 +21,91 @@ public class Spirograph extends Application {
     private TextField v2;
     private TextField v3;
     private TextField v4;
-    
+    private TextField color;
+    private Button draw;
+    private Button clear;
+
+    private Canvas canvas;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
-       
+        canvas = new Canvas(1920, 1080);
+
         VBox mainBox = new VBox();
         HBox topBar = new HBox();
         mainBox.getChildren().add(topBar);
         mainBox.getChildren().add(new Group(canvas));
-        
+
         topBar.getChildren().add(v1 = new TextField("300"));
         topBar.getChildren().add(v2 = new TextField("1"));
         topBar.getChildren().add(v3 = new TextField("300"));
         topBar.getChildren().add(v4 = new TextField("10"));
-                
-        v1.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
-        v2.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
-        v3.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
-        v4.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
-        
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
+//        v1.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
+//        v2.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
+//        v3.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
+//        v4.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
+
+        topBar.getChildren().add(color = new TextField("#000000"));
+        topBar.getChildren().add(draw = new Button("Draw"));
+        topBar.getChildren().add(clear = new Button("Clear"));
+        draw.setOnAction(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
+        clear.setOnAction(e -> clear(new FXGraphics2D(canvas.getGraphicsContext2D())));
+
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
+        graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+        graphics.scale(1, -1);
+//        draw(graphics);
         primaryStage.setScene(new Scene(mainBox));
         primaryStage.setTitle("Spirograph");
         primaryStage.show();
     }
-    
-    
+
+
     public void draw(FXGraphics2D graphics) {
         //you can use Double.parseDouble(v1.getText()) to get a double value from the first textfield
         //feel free to add more textfields or other controls if needed, but beware that swing components might clash in naming
+        clear(graphics);
+
+        double a, b, c, d;
+        try {
+            a = Integer.parseInt(v1.getText());
+            b = Integer.parseInt(v2.getText());
+            c = Integer.parseInt(v3.getText());
+            d = Integer.parseInt(v4.getText());
+            graphics.setColor(Color.decode(this.color.getText()));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        double res = 0.001;
+        double scale = 1;
+        double x1, x2, y1, y2;
+        for (double i = 0; i < Math.PI * 2; i += res) {
+            x1 = fx(i, a, b, c, d);
+            x2 = fx(i + res, a, b, c, d);
+            y1 = fy(i, a, b, c, d);
+            y2 = fy(i + res, a, b, c, d);
+            Line2D.Double line = new Line2D.Double(x1 * scale, y1 * scale, x2 * scale, y2 * scale);
+            graphics.draw(line);
+        }
     }
-    
-    
-    
+
+    private void clear(FXGraphics2D graphics) {
+        graphics.setBackground(Color.WHITE);
+        graphics.clearRect((int) (0 - this.canvas.getWidth() / 2), (int) (0 - this.canvas.getHeight() / 2), (int) this.canvas.getWidth(), (int) this.canvas.getHeight());
+        graphics.setBackground(Color.BLACK);
+    }
+
+    private double fx(double t, double a, double b, double c, double d) {
+        return a * Math.cos(b * t) + c * Math.cos(d * t);
+    }
+
+    private double fy(double t, double a, double b, double c, double d) {
+        return a * Math.sin(b * t) + c * Math.sin(d * t);
+    }
+
     public static void main(String[] args) {
         launch(Spirograph.class);
     }
