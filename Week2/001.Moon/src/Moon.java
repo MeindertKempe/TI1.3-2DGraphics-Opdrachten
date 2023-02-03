50c21394881ba4e675887d853232925dc069fc4d
Meindert Kempe <mail@mkempe.com>
2023-02-03 21:30:32 +0100

    Week 2.1 Moon CSG version

--- Week2/001.Moon/src/Moon.java
+++ Week2/001.Moon/src/Moon.java
@@ -16,8 +16,7 @@ public class Moon extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -28,16 +27,34 @@ public class Moon extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+        graphics.scale(1, -1);
+
+        Area moon = new Area(new Ellipse2D.Double(-100, -100, 200, 200));
+        Area cutout = new Area(new Ellipse2D.Double(-100 - 30, -100 + 30, 200, 200));
+        Area crescent = new Area(moon);
+        crescent.subtract(cutout);
+
+        graphics.fill(crescent);
+
+//        GeneralPath myShape = new GeneralPath();
+//        myShape.moveTo(0, -100);
+////        myShape.quadTo(200, -200, 0, -300);
+//        myShape.curveTo(200, -100, 200, -300, 0, -300);
+//        myShape.moveTo(0, -100);
+//        myShape.curveTo(-200, -100, -200, -300, 0, -300);
+//        myShape.closePath();
+//        graphics.draw(myShape);
+
+//        graphics.draw(new Arc2D.Double(canvas.getWidth()/2, canvas.getHeight()/2, 100, 100, 3, 300, Arc2D.OPEN));
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Moon.class);
     }
 
