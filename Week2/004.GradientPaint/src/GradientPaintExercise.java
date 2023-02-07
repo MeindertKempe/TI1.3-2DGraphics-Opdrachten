a5ee91117db98d4a4ccaacd035fdb9c72dc4d959
Meindert Kempe <mail@mkempe.com>
2023-02-07 10:32:40 +0100

    Week 2.4 GradientPaint

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -14,31 +14,54 @@ import org.jfree.fx.ResizableCanvas;
 
 public class GradientPaintExercise extends Application {
     private ResizableCanvas canvas;
+    private Point2D.Double center;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        center = new Point2D.Double(canvas.getWidth() / 2, canvas.getHeight() / 2);
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
+        canvas.setOnMouseDragged(e -> {
+            center = new Point2D.Double(e.getX(), e.getY());
+            draw(graphics);
+        });
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        draw(graphics);
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+    public void draw(FXGraphics2D graphics) {
+        RadialGradientPaint paint = new RadialGradientPaint(center, 200,
+                new float[]{
+                        0.1f,
+                        0.2f,
+                        0.3f,
+                        0.4f,
+                        0.5f,
+                        0.6f,
+                        0.7f,
+                        0.8f
+                },
+                new Color[]{
+                        Color.BLUE,
+                        Color.CYAN,
+                        Color.GREEN,
+                        Color.MAGENTA,
+                        Color.ORANGE,
+                        Color.PINK,
+                        Color.RED,
+                        Color.YELLOW
+                });
+        graphics.setPaint(paint);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(GradientPaintExercise.class);
     }
-
 }
