7624aaf2173e2b849a4653a277638b699ce6baba
Meindert Kempe <mail@mkempe.com>
2023-02-13 11:16:19 +0100

    Week 3.1 Rainbow

--- Week3/001.Rainbow/src/Rainbow.java
+++ Week3/001.Rainbow/src/Rainbow.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.font.GlyphVector;
 import java.awt.geom.*;
 
 import javafx.application.Application;
@@ -16,8 +17,7 @@ public class Rainbow extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -28,16 +28,45 @@ public class Rainbow extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Font font = graphics.getFont();
+
+        /* Colouring this is hard. */
+//        GlyphVector vector = font.createGlyphVector(graphics.getFontRenderContext(), "regenboog");
+//        for (int i = 0; i < vector.getNumGlyphs(); i++) {
+//            AffineTransform a = new AffineTransform();
+//            a.scale(10, 10);
+//            a.rotate((-0.5 * Math.PI) + (Math.PI / (vector.getNumGlyphs() - 1) * i));
+//            vector.setGlyphTransform(i, a);
+//        }
+//
+//        graphics.fill(vector.getOutline((float) canvas.getWidth() / 2, (float) canvas.getHeight() / 2));
+
+        char[] chars = "regenboog".toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            GlyphVector vector = font.createGlyphVector(graphics.getFontRenderContext(), Character.toString(chars[i]));
+            AffineTransform a = new AffineTransform();
+            a.scale(10, 10);
+            a.rotate((-0.5 * Math.PI) + (Math.PI / (chars.length - 1) * i));
+            Rectangle2D bounds = vector.getLogicalBounds();
+            a.translate(-bounds.getWidth() / 2, -(bounds.getHeight() / 2) - 7);
+
+            Shape letter = a.createTransformedShape(vector.getOutline());
+            letter = AffineTransform.getTranslateInstance(canvas.getWidth() / 2, canvas.getHeight() / 2).createTransformedShape(letter);
+            
+            graphics.setColor(Color.getHSBColor((1f / chars.length) * i, 1, 1));
+            graphics.fill(letter);
+            graphics.setColor(Color.black);
+            graphics.draw(letter);
+        }
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Rainbow.class);
     }
 
