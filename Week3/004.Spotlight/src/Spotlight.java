331768476743da02be27403e63748af2ca0ad7c8
Meindert Kempe <mail@mkempe.com>
2023-02-13 15:51:25 +0100

    Week 3.4 Spotlight

--- Week3/004.Spotlight/src/Spotlight.java
+++ Week3/004.Spotlight/src/Spotlight.java
@@ -2,6 +2,9 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -20,10 +23,13 @@ import org.jfree.fx.ResizableCanvas;
 
 public class Spotlight extends Application {
     private ResizableCanvas canvas;
+    private BufferedImage image;
+    private double x = 500;
+    private double y = 500;
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
+        image = ImageIO.read(Paths.get("Week3/004.Spotlight/res/1.png").toFile());
 
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
@@ -33,8 +39,7 @@ public class Spotlight extends Application {
             long last = -1;
 
             @Override
-            public void handle(long now)
-            {
+            public void handle(long now) {
                 if (last == -1)
                     last = now;
                 update((now - last) / 1000000000.0);
@@ -43,32 +48,39 @@ public class Spotlight extends Application {
             }
         }.start();
 
+        canvas.setOnMouseDragged(e -> {
+            x = e.getX();
+            y = e.getY();
+            draw(g2d);
+        });
+
         stage.setScene(new Scene(mainPane));
         stage.setTitle("Spotlight");
         stage.show();
         draw(g2d);
     }
 
-
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-    }
 
-    public void init()
-    {
-
-    }
-
-    public void update(double deltaTime)
-    {
+        Shape shape = new Ellipse2D.Double(x - 100, y - 100, 200, 200);
+        Shape clip = graphics.getClip();
+        graphics.setClip(shape);
+        graphics.drawImage(image, new AffineTransform(), null);
+        graphics.setClip(clip);
 
     }
 
-    public static void main(String[] args)
-    {
+    public void init() {
+
+    }
+
+    public void update(double deltaTime) {
+    }
+
+    public static void main(String[] args) {
         launch(Spotlight.class);
     }
 
